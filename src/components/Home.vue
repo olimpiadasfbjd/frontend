4c39128aa2122c27d57b0b00782f124cc1e7bf49
Juan Dubowez <juancruzdubowez@gmail.com>
2019-08-23 12:10:59 -0300

    view and store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,18 +5,69 @@
           <v-toolbar-title>Mediciones</v-toolbar-title>
       </v-card-title>
       <v-card-text>
-        
+        <v-card
+    class="mx-auto text-center"
+    color="green"
+    dark
+    max-width="600"
+  >
+    <v-card-text>
+      <v-sheet color="rgba(0, 0, 0, .12)">
+        <v-sparkline
+          :value="value"
+          color="rgba(255, 255, 255, .7)"
+          height="100"
+          padding="24"
+          stroke-linecap="round"
+          smooth
+        >
+          <template v-slot:label="item">
+            ${{ item.value }}
+          </template>
+        </v-sparkline>
+      </v-sheet>
+    </v-card-text>
+
+    <v-card-text>
+      <div class="display-1 font-weight-thin">Mediciones ultimas 24h</div>
+    </v-card-text>
+      </v-card>
       </v-card-text>
       </v-card>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
+
+export default {
+    data: () => ({
+      value: [
+        423,
+        446,
+        675,
+        510,
+        590,
+        610,
+        760,
+      ],
+    }),
+
+  name: 'Home',
+  computed: {
+    ...mapState(['medidas'])
+  },
+  mounted() {
+    this.$store.dispatch('medidas/obtainMedidas')
+  }
+}
 </script>
 
 <style>
 .cardMedidas {
-  border-radius: 10px;
+border-radius: 10px !important;
+padding: 10px;
+margin: 10px;
 }
 </style>
--- src/store/modules/medidas.js
+++ src/store/modules/medidas.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
 
 const state = {
+  medidas : null
 }
 const mutations = {
+  setMedidas(state,medidas){
+    state.medidas = medidas  
+  }
 }
+
 const actions = {
+  obtainMedidas({ commit }) {
+    axios({
+      method: 'get',
+      url: 'http://192.168.30.199:9090',
+    })
+    .then(res => {
+      commit('setMedidas', res.data)
+      console.log(res.data)
+    })
+  }
 }
 export default {
   namespaced: true,
